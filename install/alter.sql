626069dc7186de9f1fda13bfdd557766bcf0b322
Mikhail N Bogdanov <bogdanovmn@gmail.com>
2015-03-05 23:54:10 +0300

    minor

--- install/alter.sql
+++ install/alter.sql
@@ -1,1 +1,3 @@
-alter table personal_messages change new is_new tinyint not null default 1;
+alter table creo_stats add key i_creo_stats__creo_votes (creo_id, votes);
+
+-- alter table personal_messages change new is_new tinyint not null default 1;
