58775445f2577e45bcafed33fd0436e227fc4eb6
Mikhail N Bogdanov <bogdanovmn@gmail.com>
2015-03-06 00:07:56 +0300

    optimize creo list by period

--- install/alter.sql
+++ install/alter.sql
@@ -1,3 +1,5 @@
 alter table creo_stats add key i_creo_stats__creo_votes (creo_id, votes);
+alter table creo add column post_year year not null;
+alter table creo add key i_creo__type_year (type, post_year, post_date);
 
 -- alter table personal_messages change new is_new tinyint not null default 1;
